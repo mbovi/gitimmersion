d1b6b81b031799b502d0b5e6b1b35315a852db38
Michael <mbovi@uncc.edu>
2017-11-11 19:18:47 +0000

    Add an author/email comment

--- hello.rb
+++ hello.rb
@@ -1,3 +1,15 @@
 # Default is "World"
 name = ARGV.first || "World"
 puts "Hello, #{name}!"
+
+# Default is World
+# Author: Jim Weirich
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
+
+# Default is World
+# Author: Jim Weirich (jim@somewhere.com)
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
